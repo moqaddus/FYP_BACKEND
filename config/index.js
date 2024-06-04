6829bbd02a563d3ad90a2afed663d5406be81dd9
moqaddus <138295286+moqaddus@users.noreply.github.com>
2024-06-04 22:22:40 +0500

    Add files via upload

--- config/index.js
+++ config/index.js
@@ -5,6 +5,7 @@ const PORT=process.env.PORT;
 const CONN_STRING=process.env.CONN_STRING;
 const STRIPE_SECRET_KEY=process.env.STRIPE_SECRET_KEY;
 const FrontEnd=process.env.FrontEnd;
+const API_KEY=process.env.API_KEY
 
 
-export { PORT, CONN_STRING,STRIPE_SECRET_KEY ,FrontEnd};
+export { PORT, CONN_STRING,STRIPE_SECRET_KEY ,FrontEnd,API_KEY};
